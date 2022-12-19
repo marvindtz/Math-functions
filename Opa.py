cc40f9136173b6c412b772032acc08e8d2aaa7ba
Marvindtz <marvin@dietzm.de>
2022-12-19 22:56:32 +0100

    Random Number, Counter, Timer

--- Opa.py
+++ Opa.py
@@ -1,12 +1,19 @@
 import turtle
 import math
 import time
+import random
 
 class Rtest:
     t = turtle.Pen()
     t.speed(0)
     turtle.bgcolor("black") 
     t.pencolor("red")
+    #Ergebnis
+    start = 0
+    sprung = 0
+    treffer = 0
+    fehler = 0
+    durchschnitt = 0
     def paintKreisBogen(self, radius):   
         self.t.up() # nicht malen
         self.t.setpos(0,0)
@@ -38,11 +45,11 @@ class Rtest:
     def punkt(self,grad,radius,gefuellt):
         umfang = 2*radius*math.pi
         teilumfang = umfang / 360 * grad
-        print(f"Teil:{teilumfang}")
+      #  print(f"Teil:{teilumfang}")
         seite1 = radius*math.sin(teilumfang/radius)
         seite2 = radius*math.cos(teilumfang/radius)
         seite3 = radius
-        print(f"a:{seite1},b:{seite2},c:{seite3}")
+       # print(f"a:{seite1},b:{seite2},c:{seite3}")
         xpos = seite1
         ypos = seite2
         self.t.up()
@@ -62,12 +69,41 @@ class Rtest:
             self.t.pencolor("red")
             self.t.circle(10)
 
+    def runtest(self,radius):
+        turtle.onkey(self.getroffen, 'w')
+        turtle.listen()
+        for i in range(0, 360, 10):
+            number = random.randint(0,11)
+            if number != 10:
+                self.punkt(i,radius,True)
+                time.sleep(0.5)
+                self.punkt(i,radius,False)
+            else:
+                #Sprung
+                self.sprung = self.sprung + 1
+                self.start = time.time_ns()
+
+    def getroffen(self):
+        #Tasteninput
+        end = time.time_ns()
+        zeit = (end - self.start) / 1000 / 1000
+        if zeit <= 3000:
+            self.treffer = self.treffer + 1
+            self.durchschnitt = self.durchschnitt + zeit
+            self.start = 0
+        else:
+            self.fehler = self.fehler + 1
+            print(f"fehler:{self.fehler}")
+
+    def ergebnis(self):
+        print(f"Ergebnis:{self.sprung},{self.treffer},{self.fehler}")
+        print(f"Durchschnitt:{self.durchschnitt / self.treffer}")
 def main():
     rt = Rtest()
     #rt.paintKreisBogen(400)
     rt.kreise(400)
-    rt.punkt(70,400,True)
-    rt.punkt(70,400,False)
+    rt.runtest(400)
+    rt.ergebnis()
     input("Press key to exit")
 
 
